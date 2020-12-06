209e29085da193ea5098cb8f201f7b0bc320079a
Hamza Khuswan <hamza@golinears.com>
2020-12-06 03:48:44 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var t=document.getElementById("drop-btn"),e=document.getElementById("drop-txt");t.addEventListener("click",(function(){e.clientHeight?e.style.height=0:e.style.height=e.scrollHeight+"px"}))})();
